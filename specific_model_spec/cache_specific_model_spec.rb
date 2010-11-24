700c6c6b4669b29f3c10049b5754970cd0306e98
Li Xiao <swing1979@gmail.com>
2010-11-23 22:07:41 -0800

    more assertions for caching specific model

--- specific_model_spec/cache_specific_model_spec.rb
+++ specific_model_spec/cache_specific_model_spec.rb
@@ -18,6 +18,13 @@ module Cash
 
       story = Story.create!(:title => 'a real story')
       $memcache.get("Story:1/id/#{story.id}").should_not == nil
+
+      Story.find_by_title('a real story')
+      $memcache.get("Story:1/title/a+real+story").should_not == nil
+
+      Story.count(:conditions => "title = 'a real story'")
+      $memcache.get("Story:1/title/a+real+story/count", true).should == '1'
+      Story.count(:conditions => "title = 'a real story'").should == 1
     end
   end
 end
